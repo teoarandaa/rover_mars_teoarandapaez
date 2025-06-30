9ff0a75087a10f050eb249ff300c3784714d84a8
Teo Aranda Páez <145863288+teoarandaa@users.noreply.github.com>
2025-06-30 23:50:18 +0200

    Update rover.blade.php

--- rover_mars_teoarandapaez/resources/views/rover.blade.php
+++ rover_mars_teoarandapaez/resources/views/rover.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <title>Probar Rover en Marte - Vue.js</title>
+    <title>Rover en Marte</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
     <style>
         body {
